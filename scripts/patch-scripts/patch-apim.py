d8e80feaa281a1e7a86bc1c6937fc978e6538e9b
Chamod Samarajeewa <chamod.14@cse.mrt.ac.lk>
2020-05-30 16:00:50 +0530

    Update patch-apim script to support a single component

--- scripts/patch-scripts/patch-apim.py
+++ scripts/patch-scripts/patch-apim.py
@@ -1,4 +1,3 @@
-from git import Repo
 import os
 import subprocess
 from xml.dom import minidom
@@ -15,7 +14,7 @@ def readConfig(configPath):
     return json.loads(data)
 
 
-def extracyCarbonApimgtVersion(config):
+def extractCarbonApimgtVersion(config):
     doc = minidom.parse(os.path.join(
         config['CARBON_APIMGT_LOCATION'], 'pom.xml'))
     return doc._get_firstChild()\
@@ -25,114 +24,91 @@ def extracyCarbonApimgtVersion(config):
         .nodeValue
 
 
-def extractModifiedComponents(files):
-    print('\nExtracting modified components...')
-    components = set()
-    for file in files:
-        if file.startswith(PREFIX):
-            component = file.replace(PREFIX, '').split('/')[0]
-            components.add(component)
-    print('Modified components: ' + str([c for c in components]))
-    return components
+def extractModifiedComponent(file):
+    print('\nExtracting modified component...')
+    for subPath in file.split('/'):
+        if 'org.wso2' in subPath:
+            return subPath
+    return None
 
 
-def filterIgnoredComponents(config, components):
-    print('\nFiltering components...')
-    ignoredComponents = config['IGNORED_COMPONENTS']
-    print('Ignored components: ' + str(ignoredComponents))
-    filteredComponents = [c for c in components if c not in ignoredComponents]
-    print('Filtered componets: ' + str(filteredComponents))
-    return filteredComponents
+def buildComponent(config, component):
+    print('\nBuilding component...')
+    COMPONENT_PATH = os.path.join(
+        config['CARBON_APIMGT_LOCATION'], PREFIX, component)
+    p = subprocess.Popen(['mvn', 'clean', 'install', '-Dmaven.test.skip=true'],
+                         cwd=os.path.join(config['CARBON_APIMGT_LOCATION'],
+                                          PREFIX, COMPONENT_PATH),
+                         stdout=subprocess.PIPE)
+    status = False
+    while True:
+        output = p.stdout.readline()
+        if output:
+            print(output)
+            if not status and b'BUILD SUCCESS' in output:
+                status = True
+        if p.poll() is not None:
+            break
+
+    if status:
+        print('\nSuccesfully built <' + component + '>\n')
+    else:
+        print('\nFailed to build <' + component + '>\n')
+    return status
 
 
-def buildComponents(config, components):
-    print('\nBuilding components...')
-    for c in components:
-        COMPONENT_PATH = os.path.join(
-            config['CARBON_APIMGT_LOCATION'], PREFIX, c)
-        p = subprocess.Popen(['mvn', 'clean', 'install', '-Dmaven.test.skip=true'],
-                             cwd=os.path.join(config['CARBON_APIMGT_LOCATION'],
-                                              PREFIX, COMPONENT_PATH),
-                             stdout=subprocess.PIPE)
-        status = False
-        while True:
-            output = p.stdout.readline()
-            if output:
-                print(output)
-                if not status and b'BUILD SUCCESS' in output:
-                    status = True
-            if p.poll() is not None:
-                break
+def patchComponent(config, component, version):
+    print('\nPatching built component...')
+    COMPONENT_TARGET_PATH = os.path.join(config['CARBON_APIMGT_LOCATION'],
+                                         PREFIX, component, 'target')
 
-        if status:
-            print('\nSuccesfully built <' + c + '>\n')
-        else:
-            print('\nFailed to build <' + c + '>\n')
-            return False
-    return True
-
-
-def patchComponents(config, components, version):
-    print('\nPatching built components...')
-    for c in components:
-        COMPONENT_TARGET_PATH = os.path.join(config['CARBON_APIMGT_LOCATION'],
-                                             PREFIX, c, 'target')
-
-        for f in os.listdir(COMPONENT_TARGET_PATH):
-            source = os.path.join(COMPONENT_TARGET_PATH, f)
-            if f == c + '-' + version + '.zip' \
-                    or f == c + '-' + version + '.jar':
-                # Copy new JAR/ZIP
-                destination = os.path.join(
-                    config['PATCH_DIR'], f.replace('-', '_', 1))
+    for f in os.listdir(COMPONENT_TARGET_PATH):
+        source = os.path.join(COMPONENT_TARGET_PATH, f)
+        if f == component + '-' + version + '.zip' \
+                or f == component + '-' + version + '.jar':
+            # Copy new JAR/ZIP
+            destination = os.path.join(
+                config['PATCH_DIR'], f.replace('-', '_', 1))
+            try:
                 shutil.copyfile(source, destination)
-                print('Copied ' + source + ' to ' + destination + '\n')
-            elif f.endswith('.war'):
-                try:
-                    # Delete extracted WAR
-                    extractedWar = os.path.join(
-                        config['WEB_APP_DIR'], f.replace('.war', ''))
-                    shutil.rmtree(extractedWar)
-                    print('Deleted ' + extractedWar + '\n')
-                except FileNotFoundError:
-                    pass
-                # Copy new WAR
-                destination = os.path.join(config['WEB_APP_DIR'], f)
+            except FileNotFoundError:
+                os.mkdir(config['PATCH_DIR'])
                 shutil.copyfile(source, destination)
-                print('Copied ' + source + ' to ' + destination + '\n')
+            print('Copied ' + source + ' to ' + destination + '\n')
+        elif f.endswith('.war'):
+            try:
+                # Delete extracted WAR
+                extractedWar = os.path.join(
+                    config['WEB_APP_DIR'], f.replace('.war', ''))
+                shutil.rmtree(extractedWar)
+                print('Deleted ' + extractedWar + '\n')
+            except FileNotFoundError:
+                pass
+            # Copy new WAR
+            destination = os.path.join(config['WEB_APP_DIR'], f)
+            shutil.copyfile(source, destination)
+            print('Copied ' + source + ' to ' + destination + '\n')
 
 
-def run(configPath):
+def run(configPath, modifiedFilePath):
     config = readConfig(configPath)
-    carbonApimgtVersion = extracyCarbonApimgtVersion(config)
+    carbonApimgtVersion = extractCarbonApimgtVersion(config)
     if not carbonApimgtVersion:
         print('Failed to detect carbon.apimgt.version')
         return
     print('carbon.apimgt.version: ' + carbonApimgtVersion)
 
-    repo = Repo(config['CARBON_APIMGT_LOCATION'])
-    assert not repo.bare
-
-    modifiedFiles = [item.a_path for item in repo.index.diff(None)]
-    if not modifiedFiles:
-        print('No modified files')
-        return
-
-    components = extractModifiedComponents(modifiedFiles)
-    if not components:
+    component = extractModifiedComponent(modifiedFilePath)
+    if not component:
         print('Modified Component extraction failed')
         return
+    print('\n Component: ' + component)
 
-    components = filterIgnoredComponents(config, components)
-    if not components:
-        print('No Components after filtering ignored components')
-        return
-
-    buildStatus = buildComponents(config, components)
+    buildStatus = buildComponent(config, component)
     if not buildStatus:
         return
 
-    patchComponents(config, components, carbonApimgtVersion)
+    patchComponent(config, component, carbonApimgtVersion)
 
 
-run(sys.argv[1])
+run(sys.argv[1], sys.argv[2])
